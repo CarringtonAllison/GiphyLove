3574df29ac40e0a63eff8b4b76b3aaae0e73aae1
Carrington Rain Allison <Carrington_Allison@yahoo.com>
2018-10-16 11:16:31 -0700

    css updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,8 +11,8 @@ $("#createButton").on("click", function () {
 $(document).on("click", ".btn-secondary", function () {
     console.log($(this).attr("data-name"))
     var userSearch = $(this).attr("data-name");
-    var queryUrl = "https://api.giphy.com/v1/gifs/random?api_key=" + apiKey + "&tag=" + userSearch +"";
-    
+    var queryUrl = "https://api.giphy.com/v1/gifs/random?api_key=" + apiKey + "&tag=" + userSearch + "";
+
     // the call to pull the giphy gif. files 
     $.ajax({
         url: queryUrl,
@@ -21,13 +21,18 @@ $(document).on("click", ".btn-secondary", function () {
         console.log(response)
         //saves the gif
         var results = response.data.images.fixed_height.url;
+        var stillResults = response.data.images.fixed_height_still.url;
         console.log(results);
+        console.log(stillResults)
         var gifImage = $("<img id='gifImage'>");
         gifImage.attr("src", results);
+        gifImage.attr("data-animated", results)
+        gifImage.addClass("gif")
+        gifImage.attr("data-still", stillResults)
+        gifImage.attr("data-state", "animated")
         $("#gifGroup").prepend(gifImage);
     });
 });
-
 
 // generates buttons with a data-name the same as what the user enters 
 function renderButtons() {
@@ -39,17 +44,27 @@ function renderButtons() {
     $("#buttonGroup").append(a);
 }
 
-
 // when the clear button is clicked it removed the gifs but not the buttons
-$("#resetButton").on("click", function(){
+$("#resetButton").on("click", function () {
     event.preventDefault();
     $("#gifGroup").empty();
+    $("#buttonGroup").empty();
 })
-
 
 // when the header is clicked it removes all content
-$("#header").on("click", function(){
+$("#header").on("click", function () {
     $("#gifGroup").empty();
     $("#buttonGroup").empty();
-    
 })
+
+$(document).on("click", "#gifImage", function () {
+    console.log($(this).attr("data-state"))
+
+    if ($(this).attr("data-state") === "animated") {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still")
+    } else {
+        $(this).attr("src", $(this).attr("data-animated"));
+        $(this).attr("data-state", "animated")
+    }
+});
